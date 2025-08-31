f8c648026e431757037014e3337aff4645825e46
conao3 <conao3@gmail.com>
2025-09-01 00:17:06 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "sample-clojure-make-kanban";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, config, ... }:
+        {
+          packages.default = pkgs.hello;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              graalvm-ce
+              (clojure.override { jdk = graalvm-ce; })
+              clojure-lsp
+            ];
+          };
+
+          treefmt = {
+            programs.nixfmt.enable = true;
+            programs.cljfmt.enable = true;
+          };
+        };
+    };
+}
