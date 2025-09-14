7e798142ff711b8c2a75cdc24c3a3dd58b276f7a
conao3 <conao3@gmail.com>
2025-09-14 16:29:29 +0900

    use post

--- sections/section02/src/conao3/kanban/routes.clj
+++ sections/section02/src/conao3/kanban/routes.clj
@@ -4,12 +4,12 @@
 
 (def routes
   [["/api"
-    ["/health" c.resolver/health]
+    ["/health" {:get c.resolver/health}]
     ["/auth"
-     ["/signup" {:get c.resolver/auth-signup}]
-     ["/initiate-auth" {:get c.resolver/auth-initiate-auth}]
-     ["/refresh-token" {:get c.resolver/auth-refresh-token}]
-     ["/change-password" {:get c.resolver/auth-change-password}]
-     ["/forgot-password" {:get c.resolver/auth-forgot-password}]
-     ["/delete-user" {:get c.resolver/auth-delete-user}]]
+     ["/signup" {:post c.resolver/auth-signup}]
+     ["/initiate-auth" {:post c.resolver/auth-initiate-auth}]
+     ["/refresh-token" {:post c.resolver/auth-refresh-token}]
+     ["/change-password" {:post c.resolver/auth-change-password}]
+     ["/forgot-password" {:post c.resolver/auth-forgot-password}]
+     ["/delete-user" {:post c.resolver/auth-delete-user}]]
     ["/user/:id" {:get c.resolver/user-get :put c.resolver/user-update}]]])
