d4c4c6daae79765d9f6fcfbcb3383862e4cbf46f
conao3 <conao3@gmail.com>
2025-09-14 13:48:36 +0900

    use ring-http-response

--- sections/section02/src/conao3/kanban/resolver.clj
+++ sections/section02/src/conao3/kanban/resolver.clj
@@ -1,61 +1,53 @@
 (ns conao3.kanban.resolver
   (:require
+   [ring.util.http-response :as res]
    [clojure.data.json :as json]))
 
 (defn health [_req]
-  {:status 200
-   :body "ok"})
+  (res/ok "ok"))
 
 (defn auth-signup
   "req: :username str :password str"
   [_req]
-  {:status 200
-   :body "ok"})
+  (res/ok "ok"))
 
 (defn auth-initiate-auth
   "req: :username str :password str"
   [_req]
-  {:status 200
-   :body (json/write-str
-          {:access-token "asdfasdf"
-           :id-token "asdf"
-           :refresh-token "asdf"})})
+  (res/ok (json/write-str
+           {:access-token "asdfasdf"
+            :id-token "asdf"
+            :refresh-token "asdf"})))
 
 (defn auth-refresh-token
   "req: :refresh-token str"
   [_req]
-  {:status 200
-   :body (json/write-str
-          {:access-token "asdf"
-           :id-token "asdf"
-           :refresh-token "asdf"})})
+  (res/ok (json/write-str
+           {:access-token "asdfasdf"
+            :id-token "asdf"
+            :refresh-token "asdf"})))
 
 (defn auth-change-password
   "req: :access-token str :previous-password str :proposed-password str"
   [_req]
-  {:status 200
-   :body "ok"})
+  (res/ok "ok"))
 
 (defn auth-forgot-password
   "req: :user-name str"
   [_req]
-  {:status 200
-   :body "ok"})
+  (res/ok "ok"))
 
 (defn auth-delete-user
   "req: :access-token str"
   [_req]
-  {:status 200
-   :body "ok"})
+  (res/ok "ok"))
 
 (defn user-get
   "req: :id-token str"
   [_req]
-  {:status 200
-   :body {:name "asdf"}})
+  (res/ok {:name "asdf"}))
 
 (defn user-update
   "req: :id-token str :name str"
   [_req]
-  {:status 200
-   :body "ok"})
+  (res/ok "ok"))
