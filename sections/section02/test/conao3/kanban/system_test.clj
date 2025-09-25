e4966376b2c23f010654f0093ef0a03e1871b040
conao3 <conao3@gmail.com>
2025-09-26 07:29:57 +0900

    add system test

---
+++ sections/section02/test/conao3/kanban/system_test.clj
@@ -0,0 +1,21 @@
+(ns conao3.kanban.system-test 
+  (:require
+    [clojure.test :as t]
+    [com.stuartsierra.component :as component]
+    [conao3.kanban.system :as c.system]))
+
+(def system nil)
+
+(defn system-fixture [f]
+  (alter-var-root #'system (constantly (-> (c.system/new-system) (dissoc :server))))
+  (alter-var-root #'system component/start-system)
+  (f)
+  (alter-var-root #'system component/stop-system)
+  (alter-var-root #'system (constantly nil)))
+
+(t/use-fixtures :once system-fixture)
+
+(t/deftest system-test
+  (t/is (= {:status 200 :headers {} :body "ok"}
+           ((-> system :handler :handler)
+            {:request-method :get :uri "/api/health"}))))
